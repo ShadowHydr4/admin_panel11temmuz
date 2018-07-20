13017751369ac52a33d5effa207727ef93f5e95b
Zoom <alikocman44@gmail.com>
2018-07-20 23:37:36 +0300

    Sınav Detay için Tab menu

--- deneme.js
+++ deneme.js
@@ -1,6 +1,7 @@
 // JavaScript Document
 // script for tab steps
-    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+
+ /* $('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
 
         var href = $(e.target).attr('href');
         var $curr = $(".process-model  a[href='" + href + "']").parent();
@@ -9,5 +10,52 @@
 
         $curr.addClass("active");
         $curr.prevAll().addClass("visited");
-    });
+    }); */
 // end  script for tab steps
+
+
+
+$(document).ready(function () {
+	
+    //Initialize tooltips
+    $('.nav-tabs > li a[title]').tooltip();
+    
+    //Wizard
+    $('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
+
+        var $target = $(e.target);
+    
+        if ($target.parent().hasClass('disabled')) {
+            return false;
+        }
+		var href = $(e.target).attr('href');
+        var $curr = $(".process-model  a[href='" + href + "']").parent();
+
+        $('.process-model li').removeClass();
+
+        $curr.addClass("active");
+        $curr.prevAll().addClass("visited");   
+    });
+
+    $(".next-step").click(function (e) {
+
+        var $active = $('.wizard .nav-tabs li.active');
+        $active.next().removeClass('disabled');
+        nextTab($active);
+
+    });
+    $(".prev-step").click(function (e) {
+
+        var $active = $('.wizard .nav-tabs li.active');
+        prevTab($active);
+
+    });
+});
+
+function nextTab(elem) {
+    $(elem).next().find('a[data-toggle="tab"]').click();
+}
+function prevTab(elem) {
+    $(elem).prev().find('a[data-toggle="tab"]').click();
+}
+
